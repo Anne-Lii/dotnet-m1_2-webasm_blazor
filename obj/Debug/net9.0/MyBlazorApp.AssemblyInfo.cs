17429bc63b7416ac434491c448fe754e9f16015e
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2025-01-22 10:33:33 +0100

    lagt till konvertering för meter till fot och km/h till miles/h

--- obj/Debug/net9.0/MyBlazorApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyBlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebc4f85024f5cb17bc921c9b5e93dfe52c23e76b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fb94a2fb6ddd64c4d262a405c893b48104a6e86")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
