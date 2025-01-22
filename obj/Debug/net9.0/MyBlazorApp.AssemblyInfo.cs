ebbeac1f16397ab7d24833bfab76dee989298a00
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2025-01-22 12:05:23 +0100

    bild visas nu tillsammans med slumpmässigt vald pokemon

--- obj/Debug/net9.0/MyBlazorApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyBlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17429bc63b7416ac434491c448fe754e9f16015e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ff5d4f902cf77bcd3f736896b5a6ec746dd261a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
